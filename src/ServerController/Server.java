0e90d18f1548abdfc34fb58ce5baecf5c3708372
Elton Viana <eltonvs@outlook.com>
2017-12-06 03:00:35 -0300

    Start annotations on Server class

--- src/ServerController/Server.java
+++ src/ServerController/Server.java
@@ -15,17 +15,32 @@ import View.Session;
 import View.UNOCard;
 
 public class Server implements GameConstants {
+
+	/*@ public initially game != null;
+	  @*/
 	private /*@ spec_public nullable @*/ Game game;
+
+	/*@ public initially session != null;
+	  @*/
 	private /*@ spec_public nullable @*/ Session session;
-	private /*@ spec_public nullable @*/ Stack<UNOCard> playedCards;
-	public boolean canPlay;
-	private int mode;
+
+	/*@ public initially playedCards.size() == 1;
+	  @ public constraint \old(playedCards) == playedCards;
+	  @ public constraint \old(playedCards.size()) <= playedCards.size();
+	  @*/
+	private /*@ spec_public nullable @*/ Stack<UNOCard> playedCards = new Stack<>();
+
+	/*@ public constraint (\old(canPlay) == false) ==> canPlay == false;
+	  @*/
+	public boolean canPlay = true;
+
+	/*@ public initially mode == vsPC || mode == MANUAL;
+	  @*/
+	private /*@ spec_public @*/ int mode;
 
 	public Server() {
-
 		mode = requestMode();
 		game = new Game(mode);
-		playedCards = new Stack<>();
 
 		// First Card
 		UNOCard firstCard = game.getCard();
@@ -35,18 +50,18 @@ public class Server implements GameConstants {
 		session = new Session(game, firstCard);
 
 		game.whoseTurn();
-		canPlay = true;
 	}
 
 	//return if it's 2-Player's mode or PC-mode
+	/*@ assignable \nothing;
+	  @ ensures \result == vsPC || \result == MANUAL;
+	  @*/
 	private int requestMode() {
 
 		Object[] options = { "vs PC", "Manual", "Cancel" };
 
-		int n = JOptionPane.showOptionDialog(null,
-				"Choose a Game Mode to play", "Game Mode",
-				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
-				null, options, options[0]);
+		int n = JOptionPane.showOptionDialog(null, "Choose a Game Mode to play", "Game Mode",
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
 		if (n == 2)
 			System.exit(1);
@@ -54,8 +69,17 @@ public class Server implements GameConstants {
 		return GAMEMODES[n];
 	}
 
-	//coustom settings for the first card
-	private void modifyFirstCard(UNOCard firstCard) {
+	//custom settings for the first card
+	/*@ 	requires firstCard.getType() == WILD;
+	  @ 	assignable \nothing;
+	  @ 	ensures ((WildCard) firstCard).getWildColor() != null;
+	  @ 	ensures \old(firstCard) == firstCard;
+	  @ also
+	  @ 	requires firstCard.getType() != WILD;
+	  @ 	assignable \nothing;
+	  @ 	ensures \old(firstCard) == firstCard;
+	  @*/
+	private void modifyFirstCard(/*@ non_null @*/ UNOCard firstCard) {
 		firstCard.removeMouseListener(CARDLISTENER);
 		if (firstCard.getType() == WILD) {
 			int random = new Random().nextInt() % 4;
@@ -68,13 +92,15 @@ public class Server implements GameConstants {
 	}
 
 	//return Main Panel
-	public Session getSession() {
+	/*@ requires session != null;
+	  @ ensures \result == session;
+	  @*/
+	public /*@ pure @*/ Session getSession() {
 		return this.session;
 	}
 
-
 	//request to play a card
-	public void playThisCard(UNOCard clickedCard) {
+	public void playThisCard(/*@ non_null @*/ UNOCard clickedCard) {
 
 		// Check player's turn
 		if (!isHisTurn(clickedCard)) {
@@ -111,10 +137,8 @@ public class Server implements GameConstants {
 
 		}
 
-
-
-		if(mode==vsPC && canPlay){
-			if(game.isPCsTurn()){
+		if (mode == vsPC && canPlay) {
+			if (game.isPCsTurn()) {
 				game.playPC(peekTopCard());
 			}
 		}
@@ -130,7 +154,7 @@ public class Server implements GameConstants {
 	}
 
 	//check player's turn
-	public boolean isHisTurn(UNOCard clickedCard) {
+	public /*@ pure @*/ boolean isHisTurn(/*@ non_null @*/ UNOCard clickedCard) {
 
 		for (Player p : game.getPlayers()) {
 			if (p.hasCard(clickedCard) && p.isMyTurn())
@@ -140,11 +164,17 @@ public class Server implements GameConstants {
 	}
 
 	//check if it is a valid card
-	public boolean isValidMove(UNOCard playedCard) {
+	/*@ requires peekTopCard() != null;
+	  @ ensures \result == (playedCard.getColor().equals(peekTopCard().getColor())
+	  @ 	|| playedCard.getValue().equals(peekTopCard().getValue())
+	  @ 	|| playedCard.getType() == WILD
+	  @ 	|| (peekTopCard().getType() == WILD
+	  @ 		&& ((WildCard) peekTopCard()).getWildColor().equals(playedCard.getColor())));
+	  @*/
+	public /*@ pure @*/ boolean isValidMove(/*@ non_null @*/ UNOCard playedCard) {
 		UNOCard topCard = peekTopCard();
 
-		if (playedCard.getColor().equals(topCard.getColor())
-				|| playedCard.getValue().equals(topCard.getValue())) {
+		if (playedCard.getColor().equals(topCard.getColor()) || playedCard.getValue().equals(topCard.getValue())) {
 			return true;
 		}
 
@@ -159,7 +189,16 @@ public class Server implements GameConstants {
 	}
 
 	// ActionCards
-	private void performAction(UNOCard actionCard) {
+	/*@ 	requires game != null;
+	  @ 	requires actionCard.getValue().equals(DRAW2PLUS)
+	  @ 		|| actionCard.getValue().equals(REVERSE)
+	  @ 		|| actionCard.getValue().equals(SKIP);
+	  @ 	assignable game;
+	  @ also
+	  @ 	requires game != null;
+	  @ 	assignable \nothing;
+	  @*/
+	private void performAction(/*@ non_null @*/ UNOCard actionCard) {
 
 		// Draw2PLUS
 		if (actionCard.getValue().equals(DRAW2PLUS))
@@ -170,14 +209,15 @@ public class Server implements GameConstants {
 			game.switchTurn();
 	}
 
-	private void performWild(WildCard functionCard) {
-
+	/*@ requires functionCard.getType() == WILD;
+	  @	assignable game;
+	  @*/
+	private void performWild(/*@ non_null @*/ WildCard functionCard) {
 		//System.out.println(game.whoseTurn());
-		if(mode==1 && game.isPCsTurn()){
+		if (mode == vsPC && game.isPCsTurn()) {
 			int random = new Random().nextInt() % 4;
 			functionCard.useWildColor(UNO_COLORS[Math.abs(random)]);
-		}
-		else{
+		} else {
 
 			ArrayList<String> colors = new ArrayList<>();
 			colors.add("RED");
@@ -185,8 +225,7 @@ public class Server implements GameConstants {
 			colors.add("GREEN");
 			colors.add("YELLOW");
 
-			String chosenColor = (String) JOptionPane.showInputDialog(null,
-					"Choose a color", "Wild Card Color",
+			String chosenColor = (String) JOptionPane.showInputDialog(null, "Choose a color", "Wild Card Color",
 					JOptionPane.DEFAULT_OPTION, null, colors.toArray(), null);
 
 			functionCard.useWildColor(UNO_COLORS[colors.indexOf(chosenColor)]);
@@ -196,21 +235,32 @@ public class Server implements GameConstants {
 			game.drawPlus(4);
 	}
 
+	/*@ requires game != null;
+	  @ assignable game;
+	  @*/
 	public void requestCard() {
 		game.drawCard(peekTopCard());
 
-		if(mode==vsPC && canPlay){
-			if(game.isPCsTurn())
+		if (mode == vsPC && !game.isOver()) {
+			if (game.isPCsTurn())
 				game.playPC(peekTopCard());
 		}
 
 		session.refreshPanel();
 	}
 
-	public UNOCard peekTopCard() {
+	/*@ requires playedCards != null;
+	  @ ensures \result == playedCards.peek();
+	  @*/
+	public /*@ pure @*/ UNOCard peekTopCard() {
 		return playedCards.peek();
 	}
 
+	/*@ requires game != null;
+	  @ assignable game;
+	  @ ensures (\exists int i; 0 <= i && i < game.players.length;
+	  @ 	game.players[i].getTotalCards() <= 2 ==> game.players[i].getSaidUNO());
+	  @*/
 	public void submitSaidUNO() {
 		game.setSaidUNO();
 	}
