afbef241d5ffa333f0dcef253f410624b81451f5
Elton Viana <eltonvs@outlook.com>
2017-12-06 01:07:32 -0300

    Add annotations to MyButtonListener and MyCardListener classes

--- src/ServerController/MyCardListener.java
+++ src/ServerController/MyCardListener.java
@@ -1,39 +1,53 @@
 package ServerController;
 import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
-import org.omg.CORBA.Bounds;
-import View.MainFrame;
 import View.UNOCard;
 
 public class MyCardListener extends MouseAdapter {
-	
+
 	private /*@ spec_public nullable @*/ UNOCard sourceCard;
+
 	private /*@ spec_public nullable @*/ Server myServer;
-	
+
+	/*@ requires server != null;
+	  @ assignable server;
+	  @ ensures myServer == server;
+	  @*/
 	public void setServer(Server server){
 		myServer = server;
 	}
-	
-	public void mousePressed(MouseEvent e) {		
+
+	@Override
+	/*@ requires e != null;
+	  @ requires myServer != null;
+	  @ assignable sourceCard, myServer;
+	  @ ensures sourceCard != null;
+	  @ ensures myServer == \old(myServer);
+	  @*/
+	public void mousePressed(/*@ non_null @*/ MouseEvent e) {
 		sourceCard = (UNOCard) e.getSource();
-		
+
 		try{
 			if(myServer.canPlay)
-				myServer.playThisCard(sourceCard);			
-			
+				myServer.playThisCard(sourceCard);
+
 		}catch(NullPointerException ex){
 			ex.printStackTrace();
 		}
 	}
-	
+
 	@Override
-	public void mouseEntered(MouseEvent e) {
-		super.mouseEntered(e);		
-		
+	/*@ requires e != null;
+	  @ requires myServer != null;
+	  @ assignable sourceCard, myServer;
+	  @ ensures sourceCard != null;
+	  @ ensures myServer == \old(myServer);
+	  @*/
+	public void mouseEntered(/*@ non_null @*/ MouseEvent e) {
+		super.mouseEntered(e);
+
 		sourceCard = (UNOCard) e.getSource();
 		Point p = sourceCard.getLocation();
 		p.y -=20;
@@ -41,16 +55,23 @@ public class MyCardListener extends MouseAdapter {
 	}
 
 	@Override
-	public void mouseExited(MouseEvent e) {
+	/*@ requires e != null;
+	  @ requires myServer != null;
+	  @ assignable sourceCard, myServer;
+	  @ ensures sourceCard != null;
+	  @ ensures myServer == \old(myServer);
+	  @*/
+	public void mouseExited(/*@ non_null @*/ MouseEvent e) {
 		sourceCard = (UNOCard) e.getSource();
 		Point p = sourceCard.getLocation();
 		p.y +=20;
 		sourceCard.setLocation(p);
-	}	
+	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
-		
-	}	
+	//@ assignable \nothing;
+	public void mouseReleased(/*@ non_null @*/ MouseEvent e) {
+
+	}
 
 }
