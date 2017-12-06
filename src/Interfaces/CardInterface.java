8f3e7c1bf4fa40f19ed83124672ed455208e8c32
Elton Viana <eltonvs@outlook.com>
2017-12-06 00:21:57 -0300

    Add annotations to interfaces

--- src/Interfaces/CardInterface.java
+++ src/Interfaces/CardInterface.java
@@ -5,27 +5,48 @@ import java.awt.Dimension;
 
 public interface CardInterface {
 
-	int WIDTH = 50;
-	int HEIGHT = 75;
-	Dimension SMALL = new Dimension(WIDTH, HEIGHT);
-	Dimension MEDIUM = new Dimension(WIDTH * 2, HEIGHT * 2);
-	Dimension BIG = new Dimension(WIDTH * 3, HEIGHT * 3);
+	/*@ public constraint \old(WIDTH) == WIDTH;
+	  @*/
+	/*@ spec_public @*/ int WIDTH = 50;
+
+	/*@ public constraint \old(HEIGHT) == HEIGHT;
+	  @*/
+	/*@ spec_public @*/ int HEIGHT = 75;
+	/*@ spec_public non_null @*/ Dimension SMALL = new Dimension(WIDTH, HEIGHT);
+	/*@ spec_public non_null @*/ Dimension MEDIUM = new Dimension(WIDTH * 2, HEIGHT * 2);
+	/*@ spec_public non_null @*/ Dimension BIG = new Dimension(WIDTH * 3, HEIGHT * 3);
 
 	// Default card size
-	Dimension CARDSIZE = MEDIUM;
+	/*@ public invariant CARDSIZE == MEDIUM;
+	  @*/
+	/*@ spec_public non_null @*/ Dimension CARDSIZE = MEDIUM;
 
 	// Default offset
-	int OFFSET = 71;
+	/*@ public constraint \old(OFFSET) == OFFSET;
+	  @*/
+	/*@ spec_public @*/ int OFFSET = 71;
 
-	void setColor(Color newColor);
+	/*@ requires newColor != null;
+	  @*/
+	void setColor(/*@ non_null @*/ Color newColor);
 
+	/*@ ensures \result != null;
+	  @*/
 	/*@ pure @*/ Color getColor();
 
-	void setValue(String newValue);
+	/*@ requires newValue != null;
+	  @*/
+	void setValue(/*@ non_null @*/ String newValue);
 
+	/*@ ensures \result != null;
+	  @*/
 	/*@ pure @*/ String getValue();
 
+	/*@ requires newType == UNOConstants.NUMBERS || newType == UNOConstants.ACTION || newType == UNOConstants.WILD;
+	  @*/
 	void setType(int newType);
 
+	/*@ ensures \result == UNOConstants.NUMBERS || \result == UNOConstants.ACTION || \result == UNOConstants.WILD;
+	  @*/
 	/*@ pure @*/ int getType();
 }
